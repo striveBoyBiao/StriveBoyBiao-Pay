60db866dd72db296c6ae2ecbf082e5fb5ed81419
admin <1169763240@qq.com>
2018-12-09 14:30:07 +0800

    修改

---
+++ StriveBoyBiao-Pay/src/main/java/com/zizhuling/pay/dao/PayDao.java
@@ -0,0 +1,17 @@
+package com.zizhuling.pay.dao;
+
+import com.zizhuling.pay.bean.Pay;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.List;
+
+/**
+ * @author Exrickx
+ */
+public interface PayDao extends JpaRepository<Pay,String>, JpaSpecificationExecutor<Pay> {
+
+    List<Pay> getByStateIs(Integer state);
+
+    List<Pay> getByStateIsNotAndStateIsNot(Integer state1,Integer state2);
+}
